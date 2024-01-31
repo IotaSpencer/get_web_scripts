377711bdf7c66a8a4fc629dd029c4efcb504750e
IotaSpencer <me@iotaspencer.me>
2024-01-31 17:58:41 -0500

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='gwss',
+    version='0.0.1',
+    packages=[''],
+    package_dir={'': 'tests'},
+    url='',
+    license='MIT',
+    author='Ken Spencer / IotaSpencer',
+    author_email='me@iotaspencer.me',
+    description='get web scripts & styles'
+)
