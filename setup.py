3960c0612e371e061c9e7336b3435b0c3413ca22
IotaSpencer <me@iotaspencer.me>
2024-02-14 02:25:32 -0500

    build(setuptools): remove cliff, cement, confuse from install_requires[]

--- setup.py
+++ setup.py
@@ -14,10 +14,7 @@ setup(
     install_requires=[
         'lastversion~=3.4.6',
         'aiohttp~=3.9.2',
-        'cliff~=4.5.0',
-        'cement~=3.0.8',
         'click~=8.1.7',
-        'confuse~=2.0.1',
         'pytest~=8.0.0',
         'setuptools~=59.6.0'
     ]
