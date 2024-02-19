1c241e3435c4f9abc5e44ea734bb6b4d63095aef
IotaSpencer <me@iotaspencer.me>
2024-02-18 19:33:37 -0500

    feat: get package info via resolver.py

--- gwss/resolver.py
+++ gwss/resolver.py
@@ -2,12 +2,19 @@
 # UNPKG Resolver
 #
 # return all data needed to download scripts and styles from UNPKG
-
+import sys
 from pprint import pprint
 import yaml
 import pathlib
 from pathlib import Path
+from resolver_config import projects
 
-def resolve_pkg(package):
-    current_directory = Path(__file__).parent
-    pprint(current_directory)
+def resolve_pkg(package: str):
+    package_dict = {}
+    try:
+        package_dict = projects[package]
+    except KeyError:
+        pass
+    if package_dict == {} or package_dict is None:
+        sys.exit(1)
+
