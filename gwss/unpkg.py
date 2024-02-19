c33fdd5cca2c947df28158138d1cc9c2e785d480
IotaSpencer <me@iotaspencer.me>
2024-02-18 19:31:29 -0500

    feat: add type hinting and documentation for unpkg.py

--- gwss/unpkg.py
+++ gwss/unpkg.py
@@ -1,26 +1,58 @@
+from os import PathLike
+
 from gwss.download import download_file
 import resolver
-
+from furl import furl
 class Unpkg:
 
-    base_url = 'https://unpkg.com'
+    base_url = furl('https://unpkg.com')
 
-    def __init__(self, package, version, file, s_or_s):
+    def __init__(self, package: str, version: str, file: PathLike, s_or_s: str):
         """
-        Make sure every filename is correct before going through the entirety
-        of the function
-        :param package:
-        :param version:
-        :param file:
-        :param s_or_s:
+
+        Args:
+            package (str): The package name
+            version (str):
+            file (str | PathLike): The full filename including preceding path and excluding extension
+            s_or_s:
         """
         self.package = package
         self.version = version
         self.file = file
         self.s_or_s = s_or_s
 
-        self.url = self.base_url + "%()s"
+        self.url = self.base_url
 
 
-    def unpkg_dl(self, dest_dir):
+    def unpkg_dl(self, url: str, dest_dir: PathLike, dest_file: PathLike):
+        """
+
+        Parameters
+        ----------
+        url : str
+        dest_dir : PathLike
+        dest_file : PathLike
+
+        Returns
+        -------
+
+        """
         download_file(self.url, self.package, self.file, self.version, self.s_or_s, dest_dir)
+
+    def unpkg_url(self, *args, **kwargs) -> str:
+        """
+
+        :param args:
+        :param kwargs: see keywords
+        :keyword dir (PathLike):
+        :return:
+        """
+        # create url
+        package = self.package
+        version = self.version
+        _dir = args[2] or kwargs['dir']
+        file = args[3] or kwargs['file']
+
+
+
+        return
