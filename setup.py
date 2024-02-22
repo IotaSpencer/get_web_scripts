cafb4ca70631186b823a9c8d548d63ff9335b676
IotaSpencer <me@iotaspencer.me>
2024-02-22 07:14:52 -0500

    chore: bump version to 0.0.3
    bump version to 0.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 lines = open('requirements.txt').read().splitlines()
 setup(
     name='gwss',
-    version='0.0.2',
+    version='0.0.3',
 
     url='',
     scripts=['bin/gwss'],
