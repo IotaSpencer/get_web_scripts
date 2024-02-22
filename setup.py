a2e7e13514caac91e57e636b0deacc65fa065826
IotaSpencer <me@iotaspencer.me>
2024-02-21 22:51:42 -0500

    chore: Update project version to 0.0.2
    The 'gwss' project version has been updated from 0.0.1 to 0.0.2 in both pyproject.toml and setup.py files. This new version number reflects recent changes and improvements made to the project.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 lines = open('requirements.txt').read().splitlines()
 setup(
     name='gwss',
-    version='0.0.1',
+    version='0.0.2',
 
     url='',
     scripts=['bin/gwss'],
