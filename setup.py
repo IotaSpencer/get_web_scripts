a5c920da1b9d0d7ff5d59993cc659b317b0e0045
IotaSpencer <me@iotaspencer.me>
2024-02-23 13:12:37 -0500

    Bump version: 0.0.4 → 0.0.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 lines = open('requirements.txt').read().splitlines()
 setup(
     name='gwss',
-    version='0.0.4',
+    version='0.0.5',
 
     url='',
     scripts=['bin/gwss'],
